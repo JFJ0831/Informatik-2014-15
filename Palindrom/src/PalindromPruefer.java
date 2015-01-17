7ef42a16ae615c3dde75e7e10b9d71659901ff27
JFJ0831 <cq95678@googlemail.com>
2015-01-17 23:16:09 +0100

    update

--- Palindrom/src/PalindromPruefer.java
+++ Palindrom/src/PalindromPruefer.java
@@ -1,4 +1,43 @@
-
+/**
+ * &Uuml;berpr&uuml;fung der Eigenschaft, ein Palindrom zu sein.
+ */
 public class PalindromPruefer {
 
+	/**
+	 * &Uuml;berpr&uuml;fe, ob die &uuml;bergebene Zeichenkette
+	 * ein Palindrom ist. Hierbei soll nicht zwischen Gross- und
+	 * Kleinbuchstaben unterschieden werden.
+	 * @param eingabe Zu testende Zeichenkette.
+	 * @return <CODE>true</CODE>, wenn die Zeichenkette ein Palindrom
+	 *         ist, <CODE>false</CODE> sonst.
+	 */
+	public boolean istPalindrom(String eingabe) {
+
+		boolean ausgabe = false;
+		
+		/*
+		 * Hier Programmtext ergaenzen.
+		 */
+
+		return ausgabe;
+	}
+
+	/**
+	 * &Uuml;berpr&uuml;fe, ob die &uuml;bergebene Zeichenkette
+	 * ein Satzpalindrom ist. Hierbei soll nicht zwischen Gross- und
+	 * Kleinbuchstaben unterschieden werden.
+	 * @param eingabe Zu testende Zeichenkette.
+	 * @return <CODE>true</CODE>, wenn die Zeichenkette ein Palindrom
+	 *         ist, <CODE>false</CODE> sonst.
+	 */
+	public boolean istSatzPalindrom(String eingabe) {
+
+		boolean ausgabe = false;
+		
+		/*
+		 * Hier Programmtext ergaenzen.
+		 */
+
+		return ausgabe;
+	}
 }
