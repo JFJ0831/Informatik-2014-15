8a33fb14db111051f96ab307c63ad2ca54c93e14
JFJ0831 <cq95678@googlemail.com>
2015-01-19 03:40:13 +0100

    kommentiert

--- Palindrom/src/PalindromPruefer.java
+++ Palindrom/src/PalindromPruefer.java
@@ -13,24 +13,28 @@ public class PalindromPruefer {
 	 */
 	public boolean istPalindrom(String eingabe) {
 
-		boolean ausgabe = true;
+		boolean ausgabe = true;									// Es wird von einem Palindrom ausgegangen.
 		
-		if(eingabe == null) {
-			ausgabe = false;
+		if(eingabe == null) {									// Für eine null Eingabe wird die
+			ausgabe = false;									// Ausgabe auf false (Kein Palindrom) gesetzt. 
 			return ausgabe;
 		}
-		if(eingabe == "") {
+		if(eingabe == "") {										// Hier wird die Zeichenkette der Länge 0 herausgefiltert.
 			return ausgabe;
-		}	
-		for(int a=0,e=eingabe.length()-1; a<e; a++,e--) {
-			eingabe = eingabe.toLowerCase();
-			if(eingabe.charAt(a)!=eingabe.charAt(e)) {
-                ausgabe = false;
+		}
+		eingabe = eingabe.toLowerCase();						// Umwandlung der Zeichenkette in eine Zeichenkette 
+																// bestehend nur aus Kleinbuchstaben.
+		for(int a=0,e=eingabe.length()-1; a<e; a++,e--) {		// Setzen der Variablen a (Anfang Zeichenkette) auf 0 und der Variablen 
+																// e (Ende Zeichenkette) auf die Länge der eingegebenen Zeichenkette -1, 
+																// da dies der Indexwert des letzten Zeichens ist. 
+																// Solange a kleiner e ist, wird a um eins erhöht und e um eins verringert.
+			if(eingabe.charAt(a)!=eingabe.charAt(e)) {			// Falls a einmal nicht mit e uebereinstimmt wird
+                ausgabe = false;								// die Ausgabe auf false (Kein-Palindrom) gesetzt.
             	return ausgabe;
             }
         }
 		
-		return ausgabe;
+		return ausgabe;											// Ist a nicht (mehr) kleiner als e so wird true (Palindrom) ausgegeben.
 		
 	}
 
@@ -44,26 +48,30 @@ public class PalindromPruefer {
 	 */
 	public boolean istSatzPalindrom(String eingabe) {
 
-boolean ausgabe = true;
+		boolean ausgabe = true;									// Es wird von einem Palindrom ausgegangen.
 		
-		if(eingabe == null) {
-			ausgabe = false;
+		if(eingabe == null) {									// Für eine null Eingabe wird die
+			ausgabe = false;									// Ausgabe auf false (Kein Palindrom) gesetzt.
 			return ausgabe;
 		}
-		eingabe = eingabe.toLowerCase();
-		eingabe = eingabe.replace(" ", "");
+		eingabe = eingabe.toLowerCase();						// Umwandlung der Zeichenkette in eine Zeichenkette bestehend
+		eingabe = eingabe.replace(" ", "");						// nur aus Kleinbuchstaben und ohne übliche Interpunktionszeichen. 
 		eingabe = eingabe.replace(",", "");
 		eingabe = eingabe.replace(".", "");
 		eingabe = eingabe.replace("!", "");
 		eingabe = eingabe.replace("?", "");
 		eingabe = eingabe.replace(":", "");
 		eingabe = eingabe.replace(";", "");
-		for(int a=0,e=eingabe.length()-1; a<e; a++,e--) {
-			if(eingabe.charAt(a)!=eingabe.charAt(e)) {
-                ausgabe = false;
-            	return ausgabe;
-            }
-        }
+		
+		for(int a=0,e=eingabe.length()-1; a<e; a++,e--) {		// Setzen der Variablen a (Anfang Zeichenkette) auf 0 und der Variablen 
+																// e (Ende Zeichenkette) auf die Länge der eingegebenen Zeichenkette -1, 
+																// da dies der Indexwert des letzten Zeichens ist. 
+																// Solange a kleiner e ist, wird a um eins erhöht und e um eins verringert.
+			if(eingabe.charAt(a)!=eingabe.charAt(e)) {			// Falls a einmal nicht mit e uebereinstimmt wird
+				ausgabe = false;								// die Ausgabe auf false (Kein-Palindrom) gesetzt.
+				return ausgabe;
+			}
+		}
 		
 		return ausgabe;
 	}
